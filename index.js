001c625cc0f518ad57b011ca6571b9ff487b912d
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-19 13:04:15 +0600

    database added successfully with solve the commentcion oriobloe

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config;
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// middleware
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
-// connect to server mongodb
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.p9ba0ek.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster1.p9ba0ek.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,8 +21,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -37,10 +33,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-// Running test
 app.get("/", (req, res) => {
-  res.send("Frame Maker Server is running");
+  res.send("AirCNC Server is running..");
 });
+
 app.listen(port, () => {
-  console.log("The Port Is", port);
+  console.log(`AirCNC is running on port ${port}`);
 });
