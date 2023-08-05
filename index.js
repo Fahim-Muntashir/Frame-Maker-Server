1f644b2686f60326b94d27413305de47a10f734e
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-08-05 17:49:56 +0600

    last commit

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// middleware
 
+// middleware
 app.use(cors());
 app.use(express.json());
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({
+      error: true,
+      message: "accees token unAuthirize",
+    });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        error: true,
+        message: "accees token unAuthirize",
+      });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster1.p9ba0ek.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,23 +45,73 @@ async function run() {
   try {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-
     const allTeacherData = client.db("frame-maker").collection("teachers");
     const allCourses = client.db("frame-maker").collection("classes");
     const cartCollection = client.db("frame-maker").collection("carts");
     const allUser = client.db("frame-maker").collection("users");
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ error: true, message: "access deniyed" });
+      }
+      next();
+    };
+    // app.post for jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // all user and filter
+    app.get("/users", async (req, res) => {
+      const result = await allUser.find().toArray();
+      res.send(result);
+    });
+
+    // Update user role
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "teacher",
+        },
+      };
+      const result = await allUser.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Add User to server
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
-      const alreadyUser = await usersCollection.findOne(query);
+      const alreadyUser = await allUser.findOne(query);
       if (alreadyUser) {
         return res.send({
           message: "user is used",
         });
       }
       const result = await allUser.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const result = {
+        admin: user?.role === "admin",
+      };
       res.send(result);
     });
 
@@ -57,24 +129,23 @@ async function run() {
     });
 
     //  cart functionality
-    app.post("/carts", async (req, res) => {
-      const item = req.body;
-      console.log(item);
-      const result = await cartCollection.insertOne(item);
-      res.send(result);
-    });
 
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
-      // match email data
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
 
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
     // delete single course
     app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
@@ -96,9 +167,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("AirCNC Server is running..");
+  res.send(" Server is running..");
 });
 
 app.listen(port, () => {
-  console.log(`AirCNC is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
